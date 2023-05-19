6151f8e2fbad81c5a73063518a398bfb5a11d288
hiwg08 <hiwg08@gmail.com>
2023-05-19 15:49:12 +0900

    ㅈㄷㄱ

--- contracts/Ex1.sol
+++ contracts/Ex1.sol
@@ -2,7 +2,44 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-contract Ex1
+contract Ballot
 {
-    
+    struct students
+    {
+        uint number;
+        bytes32 a;
+    }
+
+    students public qq;
+
+    uint public a = 1;
+
+    string public st1 = "hello world!";
+
+    mapping(uint => uint) public b;
+
+    mapping(address => uint) internal ms;
+
+    bytes q;
+
+    function solve1() public
+    {
+        ms[msg.sender] = 3;
+
+        b[1] = 1;
+
+        b[2] = 333;
+
+        require(ms[msg.sender] >= 2, "wow, this is awesome!");
+    }
 }
+
+contract Ex1 is Ballot
+{
+    string internal e = st1;
+    
+
+
+    uint public wetwer = ms[msg.sender];
+}
+
